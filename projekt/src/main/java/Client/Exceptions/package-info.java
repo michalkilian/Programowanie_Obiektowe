1da4279d0ff5ba35faf96a4d5940e0066ecd7caf
michalkilian <mkilian@mielec.com.pl>
2019-02-05 22:14:49 +0100

    Create package-info files for Client packages

---
+++ projekt/src/main/java/Client/Exceptions/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Custom exceptions
+ */
+package Client.Exceptions;
---
+++ projekt/src/main/java/Client/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Client classes for the MEME generator application
+ *
+ * <p>
+ * Package contains controllers for different scenes of application, main class (ClientMain), ActiveSession class that
+ * stores information about current session (username, downloaded memes, etc) and MyImageView class to display memes in
+ * generator correctly
+ * </P>
+ *
+ * <p>
+ *     FXML Files used to generate GUI are stored in resources as well as sample memes' images and other graphics
+ * </p>
+ *
+ * @author Michal Kilian
+ * @version 1.1
+ */
+
+package Client;
